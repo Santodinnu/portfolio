69ce990eaade43b6b863810b2415b3608b813be4
santodinnu <shantoshi@gmail.com>
2024-02-06 13:48:50 -0500

    deleted few unnessary components

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -11,12 +11,8 @@ import {
 
 function Header() {
   const {isDark} = useContext(StyleContext);
-  // const viewExperience = workExperiences.display;
   const viewOpenSource = openSource.display;
   const viewSkills = skillsSection.display;
-  // const viewAchievement = achievementSection.display;
-  // const viewBlog = blogSection.display;
-  // const viewTalks = talkSection.display;
 
   return (
     <Headroom>
@@ -40,31 +36,11 @@ function Header() {
               <a href="#skills">Skills</a>
             </li>
           )}
-          {/* {viewExperience && (
-            <li>
-              <a href="#experience">Work Experiences</a>
-            </li>
-          )} */}
           {viewOpenSource && (
             <li>
               <a href="#opensource">Open Source</a>
             </li>
           )}
-          {/* {viewAchievement && (
-            <li>
-              <a href="#achievements">Achievements</a>
-            </li>
-          )}
-          {viewBlog && (
-            <li>
-              <a href="#blogs">Blogs</a>
-            </li>
-          )}
-          {viewTalks && (
-            <li>
-              <a href="#talks">Talks</a>
-            </li>
-          )} */}
           <li>
             <a href="#contact">Contact Me</a>
           </li>
